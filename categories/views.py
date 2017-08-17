9e3ada1045c359d375e13783cdd74daef3c8f867
zzbrunoBrito <bruno.brito.ln@gmail.com>
2017-08-17 16:47:30 -0300

    detail and update category

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from .forms import CategoryForm
 from .models import Category
 
@@ -20,6 +20,23 @@ class ListCategoryView(TemplateView):
 			form.save()
 		return self.render_to_response(context)
 
+class DetailCategoryView(DetailView):
+	model = Category
+	template_name = 'detail_category.html'
+
+	def get_context_data(self, **kwargs):
+		context = super(DetailCategoryView, self).get_context_data(**kwargs)
+		context['form'] = CategoryForm(self.request.POST or None, instance=context['category'])
+		return context
+
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object()
+		context = self.get_context_data(object=self.object)
+		form = context['form']
+		if form.is_valid():
+			form.save()
+		return self.render_to_response(context)
 
 
 list_category = ListCategoryView.as_view()
+detail = DetailCategoryView.as_view()
