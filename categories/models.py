c24955f49ff07ac046708f0af8a2908f73056472
zzbrunoBrito <bruno.brito.ln@gmail.com>
2017-08-24 16:29:27 -0300

    crud subcategories

--- categories/models.py
+++ categories/models.py
@@ -4,7 +4,22 @@ class Category(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descrição', max_length=400)
 
+    def __str__(self):
+    	return self.name
+
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
         ordering = ['name']
+
+	
+
+class SubCategory(models.Model):
+	name = models.CharField('Nome', max_length=50)
+	description = models.TextField('Descrição', max_length=400)
+	category = models.ForeignKey(Category, verbose_name='Categoria', null=True)
+
+	class Meta:
+		verbose_name = 'Subcategoria'
+		verbose_name_plural = 'Subcategorias'
+		ordering = ['name']
--- categories/urls.py
+++ categories/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
 	url(r'^list/$', views.list_category, name='list'),
 	url(r'^detail/(?P<pk>\d+)$', views.detail, name='detail'),
 	url(r'^delete/(?P<pk>\d+)$', views.delete, name='delete'),
+	url(r'^subcategories/list/$', views.list_subcategory, name='list_subcategory'),
+	url(r'^subcategories/detail/(?P<pk>\d+)$', views.detail_subcategory, name='detail_subcategory'),
+	url(r'^subcategories/delete/(?P<pk>\d+)$', views.delete_subcategory, name='delete_subcategory'),
 ]
