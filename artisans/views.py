8415d406ec3d7c1a42c9e6c5ab3980278e8654f9
HerissonSilva <herissonsilva@Debian-i14>
2017-08-28 01:09:32 -0300

    fix import get_object_or_404 in artisans view

--- artisans/views.py
+++ artisans/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView, DetailView, View, UpdateView
 from .models import Artisan
 from .forms import ArtisanForm
