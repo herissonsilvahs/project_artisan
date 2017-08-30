edd662527bcb5685a48f8f39ee924245a16321e1
Bruno <bruno.brito.ln@gmail.com>
2017-08-30 13:55:40 -0300

    Crud materials | Update gitignore

--- categories/forms.py
+++ categories/forms.py
@@ -1,12 +1,22 @@
 from django import forms
-from .models import Category, SubCategory
+from .models import Category, SubCategory, Material
+
 
 class CategoryForm(forms.ModelForm):
-	class Meta:
-		model = Category
-		fields = ['name', 'description']
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+
 
 class SubCategoryForm(forms.ModelForm):
-	class Meta:
-		model = SubCategory
-		fields = ['name', 'description', 'category']
+    class Meta:
+        model = SubCategory
+        fields = ['name', 'description', 'category']
+
+
+class MaterialForm(forms.ModelForm):
+    class Meta:
+        model = Material
+        fields = ['name', 'description', 'subcategory']
+
+
--- categories/models.py
+++ categories/models.py
@@ -1,25 +1,41 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descrição', max_length=400)
 
     def __str__(self):
-    	return self.name
+        return self.name
 
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
         ordering = ['name']
 
-	
 
 class SubCategory(models.Model):
-	name = models.CharField('Nome', max_length=50)
-	description = models.TextField('Descrição', max_length=400)
-	category = models.ForeignKey(Category, verbose_name='Categoria', null=True, on_delete=models.SET_NULL)
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descrição', max_length=400)
+    category = models.ForeignKey(Category, verbose_name='Categoria', null=True, on_delete=models.SET_NULL)
 
-	class Meta:
-		verbose_name = 'Subcategoria'
-		verbose_name_plural = 'Subcategorias'
-		ordering = ['name']
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Subcategoria'
+        verbose_name_plural = 'Subcategorias'
+        ordering = ['name']
+
+
+class Material(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descrição')
+    subcategory = models.ForeignKey(SubCategory, verbose_name='SubCategoria', null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = 'Material'
+        verbose_name_plural = 'Materiais'
+        ordering = ['name']
+
+
