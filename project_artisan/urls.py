83a1c71c58aef4d22cfec0a808dc20f9ad256ed4
leonardfreitas <leonardofreitas159@gmail.com>
2017-08-31 17:12:07 -0300

    include index

--- project_artisan/urls.py
+++ project_artisan/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('core.urls', namespace='core') ),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^artisans/', include('artisans.urls', namespace='artisans')),
     url(r'^addresses/', include('addresses.urls', namespace='addresses')),
