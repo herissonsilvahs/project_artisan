e4d194d9481eb2f54940907d6421d062c1e46c57
Bruno <bruno.brito.ln@gmail.com>
2017-09-21 16:55:53 -0300

    comment created_at

--- comments/models.py
+++ comments/models.py
@@ -7,3 +7,4 @@ class Comment(models.Model):
     name = models.CharField('Nome', max_length=150)
     comment = models.TextField('Comentario')
     artifact = models.ForeignKey(Artifact, verbose_name='Artesanato', on_delete=models.CASCADE, null=True, related_name='comment')
+    created = models.DateTimeField('Criado em', auto_now_add=True)
