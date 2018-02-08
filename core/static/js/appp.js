6eeab75f3241d825ac066ed43f17f06791d656e8
lelukas <lelukass@gmail.com>
2018-02-08 13:34:47 -0300

    refactor menu icon's animation

--- core/static/js/appp.js
+++ core/static/js/appp.js
@@ -1,19 +1,22 @@
 function menuAnim() {
-    TweenLite.to('.menu', 1 , {transform: 'scaleY(1)', ease: Power3.easeOut});
-    TweenLite.to('.menu__bg', 1 , {css: {transform: 'scaleY(1)', ease: Power3.easeOut}, delay: 0.2});
-    TweenMax.staggerTo('.menu__item', 0.5, {css:{ transform: 'translateX(5px)', opacity: 1}, delay: 0.5}, 0.1)
+    let tl = new TimelineLite();
+
+    tl.to('.menu', 1 , {transform: 'scaleY(1)', ease: Power3.easeOut});
+    tl.to('.menu__bg', 1 , {transform: 'scaleY(1)', ease: Power3.easeOut}, "-=0.6");
+    tl.staggerTo('.menu__item', 0.5, { transform: 'translateX(5px)', opacity: 1}, 0.2 , 0.5)
 }
 
 function menuIconAnim() {
-    TweenMax.staggerTo('.menu-btn__rect:not(.menu-btn__rect--diag)', 0.2, { transform: 'translateX(60px)', opacity: 0, ease: Power2.easeIn}, 0.1);
-    TweenMax.to('.menu-btn__rect--diag', 0.5, {css: { opacity: 1, ease: Power2.easeIn}, delay: 0.6}, 0.5)
+    let tl = new TimelineLite();
 
+    tl.staggerTo('.menu-btn__rect:not(.menu-btn__rect--diag)', 0.2, { transform: 'translateX(60px)', opacity: 0, ease: Power2.easeIn}, 0.1);
+    tl.to('.menu-btn__rect--diag', 0.5, { onComplete: () => {
+        $('.menu-btn__rect--diag:nth-child(4)').css({'transform': 'rotate(45deg) scaleX(1)', 'border-radius': '10px'})
+        $('.menu-btn__rect--diag:nth-child(5)').css({'transform': 'rotate(-45deg) scaleX(1)', 'border-radius': '10px'})
+    }}, "-=0.4");
 }
 
 $('.menu-btn').click(function () {
     menuIconAnim();
     menuAnim();
-})
-
-
-//todo: verify if timeline is neccessary
+});
