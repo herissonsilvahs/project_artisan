3ef55263ab9e4e1038a31c08905ebccd6a6c55ca
lelukas <lelukass@gmail.com>
2018-02-09 11:23:16 -0300

    refactor menu and its icon animation
    Animation for X icon to appear is now manipulated through width property

--- core/static/js/appp.js
+++ core/static/js/appp.js
@@ -1,22 +1,34 @@
-function menuAnim() {
-    let tl = new TimelineLite();
+let isMenuOpened = false;
 
-    tl.to('.menu', 1 , {transform: 'scaleY(1)', ease: Power3.easeOut});
-    tl.to('.menu__bg', 1 , {transform: 'scaleY(1)', ease: Power3.easeOut}, "-=0.6");
-    tl.staggerTo('.menu__item', 0.5, { transform: 'translateX(5px)', opacity: 1}, 0.2 , 0.5)
+function tlMenuIcon() {
+    let tlMenuIcon = new TimelineLite();
+    tlMenuIcon.add(TweenMax.staggerTo('.menu-btn__rect:not(.menu-btn__rect--diag)', 0.2, {transform: 'translateX(60px)', opacity: 0, ease: Power2.easeInOut}, 0.1));
+    tlMenuIcon.add(TweenLite.to('.menu-btn__rect--diag', 0.5, {width:"100%", opacity: 1, ease: Power2.easeInOut}), "0.4");
+    return tlMenuIcon
 }
 
-function menuIconAnim() {
-    let tl = new TimelineLite();
-
-    tl.staggerTo('.menu-btn__rect:not(.menu-btn__rect--diag)', 0.2, { transform: 'translateX(60px)', opacity: 0, ease: Power2.easeIn}, 0.1);
-    tl.to('.menu-btn__rect--diag', 0.5, { onComplete: () => {
-        $('.menu-btn__rect--diag:nth-child(4)').css({'transform': 'rotate(45deg) scaleX(1)', 'border-radius': '10px'})
-        $('.menu-btn__rect--diag:nth-child(5)').css({'transform': 'rotate(-45deg) scaleX(1)', 'border-radius': '10px'})
-    }}, "-=0.4");
+function tlMenu() {
+    let tlMenu = new TimelineLite();
+    tlMenu.add(TweenLite.to('.menu', 1, {transform: 'scaleY(1)', ease: Power3.easeOut})); //red frontground
+    tlMenu.add(TweenLite.to('.menu__bg', 1, {transform: 'scaleY(1)', ease: Power3.easeOut}), "-=0.6"); // blue background
+    tlMenu.add(TweenMax.staggerTo('.menu__item', 0.5, {transform: 'translateX(5px)', opacity: 1}, 0.2), 0.5);
+    return tlMenu
 }
+
+let tlToggleMenu = new TimelineLite({paused:true});
+tlToggleMenu.add(tlMenuIcon()).add(tlMenu(), "-=0.7");
+
 
 $('.menu-btn').click(function () {
-    menuIconAnim();
-    menuAnim();
+
+    if (isMenuOpened) {
+        tlToggleMenu.reverse();
+        isMenuOpened = false
+    } else {
+        tlToggleMenu.play();
+        isMenuOpened = true
+    }
 });
+
+
+// transform: 'matrix(0.70711, 0.7071, -0.7071, 0.70711, 0, 0)'
