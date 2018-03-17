0addfdc205dbb297e085d786d803e489d70ed673
lelukas <lelukass@gmail.com>
2018-03-17 16:05:16 -0300

    create artisan page

--- project_artisan/settings.py
+++ project_artisan/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_extensions',
+    # 'django_extensions',
 
     #apps externas
     'widget_tweaks',
