c416f29d7c1a2bb9976a79be3ea73dd21cd4be41
lelukas <lelukass@gmail.com>
2018-03-18 17:52:08 -0300

    create search button and its card

--- core/static/js/appp.js
+++ core/static/js/appp.js
@@ -1,9 +1,24 @@
 let isMenuOpened = false;
+let isDimmerVisible = false;
+
+let tlToggleMenu = new TimelineLite({paused: true});
+tlToggleMenu.add(tlMenuIcon()).add(tlMenu(), "-=0.7");
+
+let tlSearchButton = new TimelineLite({paused: true});
+tlSearchButton.add(tlSearch());
 
 function tlMenuIcon() {
     let tlMenuIcon = new TimelineLite();
-    tlMenuIcon.add(TweenMax.staggerTo('.menu-btn__rect:not(.menu-btn__rect--diag)', 0.2, {transform: 'translateX(60px)', opacity: 0, ease: Power2.easeInOut}, 0.1));
-    tlMenuIcon.add(TweenLite.to('.menu-btn__rect--diag', 0.5, {width:"100%", opacity: 1, ease: Power2.easeInOut}), "0.4");
+    tlMenuIcon.add(TweenMax.staggerTo('.menu-btn__rect:not(.menu-btn__rect--diag)', 0.2, {
+        transform: 'translateX(60px)',
+        opacity: 0,
+        ease: Power2.easeInOut
+    }, 0.1));
+    tlMenuIcon.add(TweenLite.to('.menu-btn__rect--diag', 0.5, {
+        width: "100%",
+        opacity: 1,
+        ease: Power2.easeInOut
+    }), "0.4");
     return tlMenuIcon
 }
 
@@ -15,8 +30,13 @@ function tlMenu() {
     return tlMenu
 }
 
-let tlToggleMenu = new TimelineLite({paused:true});
-tlToggleMenu.add(tlMenuIcon()).add(tlMenu(), "-=0.7");
+function tlSearch() {
+    let tlSearch = new TimelineLite();
+    let cardHeight = $('.card--search').height();
+    tlSearch.add(TweenLite.to('.dimmer', 0.4, {opacity: 1, pointerEvents: 'auto', ease: Power2.easeOut}));
+    tlSearch.add(TweenLite.to('.card--search', 0.4, {transform: `translateY(-${cardHeight}px)`, ease: Power2.easeOut}),"-=0.2");
+    return tlSearch
+}
 
 
 $('.menu-btn').click(function () {
@@ -30,6 +50,16 @@ $('.menu-btn').click(function () {
         tlToggleMenu.play();
         isMenuOpened = true
     }
+});
+
+$('.search').click(function () {
+    tlSearchButton.play();
+    isDimmerVisible = true;
+});
+
+$('.dimmer').click(function () {
+    tlSearchButton.reverse();
+    isDimmerVisible = false;
 });
 
 
